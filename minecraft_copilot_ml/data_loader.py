fix: adding the loss mask to the loss computation

minecraft_copilot_ml/data_loader.py
@@ -47,7 +47,7 @@ list_of_forbidden_files = [
 
 def create_noisy_block_map(
     block_map: np.ndarray,
-) -> np.ndarray:
+) -> Tuple[np.ndarray, Tuple[int, int, int, int, int, int]]:
     x_start = np.random.randint(0, block_map.shape[0] - 1) if block_map.shape[0] > 1 else 0
     y_start = np.random.randint(0, block_map.shape[1] - 1) if block_map.shape[1] > 1 else 0
     z_start = np.random.randint(0, block_map.shape[2] - 1) if block_map.shape[2] > 1 else 0
@@ -56,7 +56,14 @@ def create_noisy_block_map(
     z_end = np.random.randint(z_start + 1, block_map.shape[2]) if block_map.shape[2] > 1 else 1
     result_block_map = block_map.copy()
     result_block_map[x_start:x_end, y_start:y_end, z_start:z_end] = "minecraft:air"
-    return result_block_map
+    return result_block_map, (
+        x_start,
+        y_start,
+        z_start,
+        x_end - x_start,
+        y_end - y_start,
+        z_end - z_start,
+    )
 
 
 def litematic_to_numpy_minecraft_map(
@@ -181,7 +188,7 @@ class MinecraftSchematicsDataset(Dataset):
     def __len__(self) -> int:
         return len(self.schematics_list_files)
 
-    def __getitem__(self, idx: int) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+    def __getitem__(self, idx: int) -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
         nbt_file = self.schematics_list_files[idx]
         numpy_minecraft_map = nbt_to_numpy_minecraft_map(nbt_file)
         block_map, (
@@ -197,20 +204,33 @@ class MinecraftSchematicsDataset(Dataset):
             random_y_height_value : random_y_height_value + minimum_height,
             random_roll_z_value : random_roll_z_value + minimum_depth,
         ]
-        noisy_focused_block_map = create_noisy_block_map(focused_block_map)
+        result_block_map, (
+            noisy_x_start,
+            noisy_y_start,
+            noisy_z_start,
+            noisy_x_width,
+            noisy_y_height,
+            noisy_z_depth,
+        ) = create_noisy_block_map(focused_block_map)
         noisy_block_map = block_map.copy()
         noisy_block_map[
             random_roll_x_value : random_roll_x_value + minimum_width,
             random_y_height_value : random_y_height_value + minimum_height,
             random_roll_z_value : random_roll_z_value + minimum_depth,
-        ] = noisy_focused_block_map
+        ] = result_block_map
         mask = np.zeros((16, 16, 16), dtype=bool)
         mask[
             random_roll_x_value : random_roll_x_value + minimum_width,
             random_y_height_value : random_y_height_value + minimum_height,
             random_roll_z_value : random_roll_z_value + minimum_depth,
         ] = True
-        return block_map, noisy_block_map, mask
+        loss_mask = np.zeros((16, 16, 16), dtype=bool)
+        loss_mask[
+            random_roll_x_value + noisy_x_start : random_roll_x_value + noisy_x_start + noisy_x_width,
+            random_y_height_value + noisy_y_start : random_y_height_value + noisy_y_start + noisy_y_height,
+            random_roll_z_value + noisy_z_start : random_roll_z_value + noisy_z_start + noisy_z_depth,
+        ] = True
+        return block_map, noisy_block_map, mask, loss_mask
 
 
 def list_schematic_files_in_folder(path_to_schematics: str) -> list[str]:

minecraft_copilot_ml/model.py
@@ -69,11 +69,14 @@ class UNet3d(pl.LightningModule):
         reconstruction = F.softmax(reconstruction, dim=1)
         return reconstruction
 
-    def step(self, batch: Tuple[np.ndarray, np.ndarray, np.ndarray], batch_idx: int, mode: str) -> torch.Tensor:
-        block_maps, noisy_block_maps, masks = batch
+    def step(
+        self, batch: Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray], batch_idx: int, mode: str
+    ) -> torch.Tensor:
+        block_maps, noisy_block_maps, masks, loss_masks = batch
         pre_processed_block_maps = self.pre_process(block_maps)
         pre_processed_noisy_block_maps = self.pre_process(noisy_block_maps).float().unsqueeze(1)
         tensor_masks = torch.from_numpy(masks).float().to("cuda" if torch.cuda.is_available() else "cpu").long()
+        tensor_loss_masks = torch.from_numpy(loss_masks).float().to("cuda" if torch.cuda.is_available() else "cpu").long()
         reconstruction = self.ml_core(pre_processed_noisy_block_maps)
 
         # Compute accuracy
@@ -84,6 +87,7 @@ class UNet3d(pl.LightningModule):
         # Compute reconstruction loss using categorical cross-entropy
         reconstruction_loss = F.cross_entropy(reconstruction, pre_processed_block_maps, reduction="none")
         reconstruction_loss = reconstruction_loss * tensor_masks
+        reconstruction_loss = reconstruction_loss * tensor_loss_masks
         reconstruction_loss = reconstruction_loss * self.unique_counts_coefficients[pre_processed_block_maps]
         reconstruction_loss = reconstruction_loss.mean()
 